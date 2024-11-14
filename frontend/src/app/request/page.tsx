d3f137753ef96c6b98c5f8d656ec6ba1d71281e0
4510nasu <4510nasu@gmail.com>
2024-11-14 14:30:06 +0900

    申請ページ追加

---
+++ frontend/src/app/request/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from 'next/link'
+
+//import { NextPage } from "next";
+
+const 変数名 = () => {
+    const va = 1;
+    return (
+      <>
+       申請画面表示予定地
+      </>
+    );
+  }
+  export default 変数名;
+  
