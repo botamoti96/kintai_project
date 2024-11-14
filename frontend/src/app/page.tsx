dbe9e0bc8ea88a5f4e64a0f2ddb1725f4ef6dfac
matsuda13 <genkokurei3@gmail.com>
2024-11-14 16:26:25 +0900

    create sampletable

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,22 @@
 import Link from 'next/link'
 
 const Home = () => {
+  const v = 1;
   return (
     <div>
       まっくろなページです<br/>
       まっくろなページです<br/>
-      まっくろなページです<br/>
+      まっくろなページで...す<br/>
+      {v}
       <Link href="/test">
         Go to Test Page
       </Link>
       <br></br>
       <Link href="/test2">
         テストページ2へ
+      </Link>
+      <Link href="/showAttendanceRecord">
+        出勤簿ページへ
       </Link>
     </div>
   );
