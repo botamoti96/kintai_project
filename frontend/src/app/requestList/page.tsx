d4c5ad0317c8fb3a1d439a4a1bde20ee1bbe1b1c
4510nasu <4510nasu@gmail.com>
2024-11-26 13:12:12 +0900

    リクエストページレイアウト更新

--- frontend/src/app/requestList/page.tsx
+++ frontend/src/app/requestList/page.tsx
@@ -1,22 +1,154 @@
-import React from "react";
+"use client";
+
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import styles from "./page.module.css";
 
 import { NextPage } from "next";
+import AttendanceRecordType from "../types/AttendanceRecordType";
+import CreateTableFunc from "../showAttendanceRecord/function/CreateTable";
 
+//ウィンドウの右上に配置するボタン
+const WindowButton = () => {
+  return (
+    <div className={styles.windowButtons}>
+      {/* ウィンドウボタン：黄色、緑、赤 */}
+      <div
+        className={styles.windowButton}
+        style={{ backgroundColor: "yellow" }}
+      ></div>
+      <div
+        className={styles.windowButton}
+        style={{ backgroundColor: "green" }}
+      ></div>
+      <div
+        className={styles.windowButton}
+        style={{ backgroundColor: "red" }}
+      ></div>
+    </div>
+  );
+};
+
+//ウィンドウの左上に配置するメニューバー
+const MenuBar = () => {
+  // プルダウンメニューの表示状態を管理
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false); // メニューが開いているかどうか
+  const menuRef = useRef<HTMLDivElement | null>(null); // メニューの参照を保持
+  return (
+    <div>
+      <div className={styles.menuBar}>
+        <div
+          className={styles.menuIcon}
+          onClick={() => setIsMenuOpen(!isMenuOpen)} // メニューアイコンをクリックした時に開閉
+        >
+          &#9776;
+        </div>
+
+        {/* プルダウンメニュー */}
+        {isMenuOpen && (
+          <div ref={menuRef} className={styles.dropdownMenu}>
+            <Link href="../time_card">
+              <div className={styles.menuItem}>出退勤打刻</div>
+            </Link>
+            <Link href="../showAttendanceRecord">
+              <div className={styles.menuItem}>出勤簿履歴表示</div>
+            </Link>
+            <Link href="../request">
+              <div className={styles.menuItem}>新規申請書の作成</div>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+//タイトル
 const Header = () => {
-  return <h1 className="underline">申請履歴一覧になる予定です</h1>;
+  return <h1 className="underline">申請承認履歴一覧</h1>;
 };
 
 const Homeback = () => {
   return <Link href="/">ホームに戻る</Link>;
 };
+
+const HandleSelectedYearMonth = () => {
+  const [selectedYear, setSelectedYear] = useState("2024");
+  const [selectedMonth, setSelectedMonth] = useState("11");
+
+  const [data, setData] = useState<AttendanceRecordType[]>(
+    CreateTableFunc(new Date(0, 0, 0), "1")
+  );
+
+  useEffect(() => {
+    if (selectedYear != "xxxx" && selectedMonth != "xx") {
+      console.log(selectedYear + "/" + selectedMonth);
+      //月を指定するときは-1しないといけない
+      //1月を表現するなら、new Dateの引数には0と入れる
+      setData(
+        CreateTableFunc(
+          new Date(Number(selectedYear), Number(selectedMonth) - 1, 1),
+          "1"
+        )
+      );
+    }
+  }, [selectedYear, selectedMonth]);
+
+  const handleSelectedYear = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedYear(event.target.value);
+  };
+
+  const handleSelectedMonth = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedMonth(event.target.value);
+  };
+
+  return (
+    <>
+      <div id="div_selectmenu">
+        <select name="year" onChange={handleSelectedYear}>
+          <option value="" hidden>
+            年
+          </option>
+          <option value="2022">2022</option>
+          <option value="2023">2023</option>
+          <option value="2024">2024</option>
+        </select>
+        <select name="month" defaultValue="月" onChange={handleSelectedMonth}>
+          <option value="" hidden>
+            月
+          </option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+          <option>9</option>
+          <option>10</option>
+          <option>11</option>
+          <option>12</option>
+        </select>
+      </div>
+    </>
+  );
+};
+
 const requestPage = () => {
   return (
-    <div className={styles.all}>
+    <div className={styles.container}>
+      <MenuBar />
       <Header />
+      <div className={styles.header}>
+        <WindowButton />
+        <HandleSelectedYearMonth />
+      </div>
+
       <br></br>
+
       <RequestTable />
+
       <br></br>
       <Homeback />
     </div>
@@ -75,9 +207,9 @@ const RequestTable = () => {
   return (
     <>
       {date.map((item, index) => (
-        <div key={item.requestId}>
+        <div key={item.requestId} className={styles.requestContainer}>
           <table className={styles.table}>
-            <caption className="black">
+            <caption className={styles.caption}>
               {date[index].year}年 {date[index].month}月 勤務実績通知書
             </caption>
             <thead className={styles.abc}>
@@ -159,7 +291,7 @@ const RequestTable = () => {
           </table>
           <br></br>
           {/*承認されているかの判定↓*/}
-          <button className={styles.kh}>
+          <button className={styles.label}>
             {(() => {
               if (date[index].isApproved === 0) {
                 return <div>承認待ち</div>;
